ff26a6acaeabd709da12012089e7457eafc3c91f
RusuDDD <rusuddd@gmail.com>
2022-09-15 22:42:01 +0200

    finish

---
+++ _22_09_15_homework_innerClass/src/Person.java
@@ -0,0 +1,59 @@
+import java.time.LocalDate;
+
+public class Person {
+
+    private final String name;
+    private final String surName;
+    private final Integer birthYear;
+
+    public Person(String name, String surname, Integer birthYear) {
+        this.name = name;
+        this.surName = surname;
+        this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surName;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surName + '\'' +
+                ", birthYear=" + birthYear +
+                '}';
+    }
+    private boolean validityDate() {
+        LocalDate date = LocalDate.of(2022, 12, 31);
+        return date.getYear() - birthYear <= 10;
+    }
+
+    public class Passport {
+        private final int passNum;
+        private final String country;
+
+        public Passport(int passNum, String country) {
+            this.passNum = passNum;
+            this.country = country;
+        }
+
+        @Override
+        public String toString() {
+            return Person.this.name + " " + Person.this.surName + ", passnum "+  passNum + ", country=" + country;
+        }
+
+        public boolean validityCheck() {
+            LocalDate date = LocalDate.of(2022, 12, 31);
+            return date.getYear() - Person.this.birthYear <= 110;
+        }
+    }
+}
---
+++ _22_09_15_homework_innerClass/src/homework.java
@@ -0,0 +1,9 @@
+public class homework {
+
+    public static void main(String[] args) {
+
+        Person.Passport passport11 = new Person("Misha", "Guzgan", 1965).new Passport(65465135, "BE");
+        System.out.println(passport11.validityCheck());
+        System.out.println(passport11);
+    }
+}
